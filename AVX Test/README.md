7b3cf669aff6db586afc46dfa86fd5ae40ce08ec
bigleux <bigleuxenchef@gmail.com>
2017-06-11 23:32:46 -0400

    upd

--- AVX Test/README.md
+++ AVX Test/README.md
@@ -32,86 +32,205 @@ Most modern c++ compiler can automatically vectorize loops. In the example here 
  
 ### Results single Precision
 
+### Compiler options : NO optimization
 
 ```
-!!!Hello World OF Advanced Vector Extensions : Single Precision Test!!!
+**** Incremental Build of configuration Debug for project AVX Test ****
+make all 
+Building file: ../src/AVX Test Single Precision.cpp
+Invoking: Cross G++ Compiler
+g++ -std=c++0x -mxsave -mavx -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/AVX Test Single Precision.d" -MT"src/AVX\ Test\ Single\ Precision.d" -o "src/AVX Test Single Precision.o" "../src/AVX Test Single Precision.cpp"
+Finished building: ../src/AVX Test Single Precision.cpp
+ 
+Building target: AVX Test
+Invoking: Cross G++ Linker
+g++  -o "AVX Test"  ./src/AVX\ Test\ Single\ Precision.o   
+Finished building target: AVX Test
+```
+#### Results
+
+```
+!!!Hello World OF Advanced Vector Extensions for : f sizeof  : 4
 Effective Threads Runtime
- 1.08624	seconds normal
- 0.40506	seconds sse128
- 0.22753	seconds sse256
+********* SQRT TEST ************
+normal_sqrt	 1.18230 s
+sse128_sqrt	 0.76245 s
+sse256_sqrt	 0.40706 s
+********* ADD  TEST ************
+normal_add	 1.56866 s
+sse128_add	 0.39155 s
+sse256_add	 0.20484 s
+********* SUB  TEST ************
+normal_sub	 1.35702 s
+sse128_sub	 0.49698 s
+sse256_sub	 0.29242 s
+********* DIV  TEST ************
+normal_div	 1.79099 s
+sse128_div	 0.44068 s
+sse256_div	 0.24973 s
+
 Effective clock Runtime
-Normal 1.1088036 s
-SSE128 0.405116 s
-SSE256 0.227802 s
 
-```
-
-```
-# with maximum optimization and -ttree-vectorize
-
-!!!Hello World OF Advanced Vector Extensions : Single Precision Test!!!
+Memory 0.     23 s
+Init   2.2707667 s
+********* SQRT TEST ************
+normal 1.1185392 s
+sse128 0. 763030 s
+sse256 0. 407841 s
+********* ADD  TEST ************
+normal 1.1571739 s
+sse128 0. 391574 s
+sse256 0. 204908 s
+********* SUB  TEST ************
+normal 1.1357153 s
+sse128 0. 499326 s
+sse256 0. 293546 s
+********* DIV  TEST ************
+normal 1.1807887 s
+sse128 0. 442165 s
+sse256 0. 251021 s
+!!!Hello World OF Advanced Vector Extensions for : d sizeof  : 8
 Effective Threads Runtime
- 0.53997	seconds normal
- 0.18437	seconds sse128
- 0.17486	seconds sse256
+********* SQRT TEST ************
+normal_sqrt	 2.45592 s
+sse128_sqrt	 1.69520 s
+sse256_sqrt	 0.88048 s
+********* ADD  TEST ************
+normal_add	 1.92358 s
+sse128_add	 1.03729 s
+sse256_add	 0.55327 s
+********* SUB  TEST ************
+normal_sub	 1.87122 s
+sse128_sub	 1.04833 s
+sse256_sub	 0.57651 s
+********* DIV  TEST ************
+normal_div	 1.90913 s
+sse128_div	 1.12137 s
+sse256_div	 0.52255 s
+
 Effective clock Runtime
-Normal 0.541213 s
-SSE128 0.184862 s
-SSE256 0.174922 s
+
+Memory 0.     21 s
+Init   4.4712237 s
+********* SQRT TEST ************
+normal 2.2469487 s
+sse128 1.1697042 s
+sse256 0. 881229 s
+********* ADD  TEST ************
+normal 1.1937319 s
+sse128 1.1041524 s
+sse256 0. 559515 s
+********* SUB  TEST ************
+normal 1.1873779 s
+sse128 1.1053096 s
+sse256 0. 579335 s
+********* DIV  TEST ************
+normal 1.1916469 s
+sse128 1.1140102 s
+sse256 0. 524420 s
+
 ```
-
-### Results Double Precision
-
-
-
-
+### Compiler options : all optimization
 
 ```
-# no compiler optimization enforce the vectorization disable to compare with and without optimization
-!!!Hello World OF Advanced Vector Extensions : Double Precision!!!
+**** Incremental Build of configuration Debug for project AVX Test ****
+make all 
+Building file: ../src/AVX Test Single Precision.cpp
+Invoking: Cross G++ Compiler
+g++ -std=c++0x -mxsave -mavx -O3 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/AVX Test Single Precision.d" -MT"src/AVX\ Test\ Single\ Precision.d" -o "src/AVX Test Single Precision.o" "../src/AVX Test Single Precision.cpp"
+Finished building: ../src/AVX Test Single Precision.cpp
+ 
+Building target: AVX Test
+Invoking: Cross G++ Linker
+g++  -o "AVX Test"  ./src/AVX\ Test\ Single\ Precision.o   
+Finished building target: AVX Test
+```
+#### Resutls
+```
+!!!Hello World OF Advanced Vector Extensions for : f sizeof  : 4
 Effective Threads Runtime
- 1.38743	seconds normal
- 0.82126	seconds sse128
- 0.51806	seconds sse256
+********* SQRT TEST ************
+normal_sqrt	 0.48298 s
+sse128_sqrt	 0.17059 s
+sse256_sqrt	 0.15783 s
+********* ADD  TEST ************
+normal_add	 0.12038 s
+sse128_add	 0.13360 s
+sse256_add	 0.12077 s
+********* SUB  TEST ************
+normal_sub	 0.12020 s
+sse128_sub	 0.12829 s
+sse256_sub	 0.12408 s
+********* DIV  TEST ************
+normal_div	 0.12853 s
+sse128_div	 0.13228 s
+sse256_div	 0.13614 s
+
 Effective clock Runtime
-Normal 1.1387632 s
-SSE128 0.821300 s
-SSE256 0.518172 s
+
+Memory 0.     25 s
+Init   1.1123633 s
+********* SQRT TEST ************
+normal 0. 483011 s
+sse128 0. 170588 s
+sse256 0. 157825 s
+********* ADD  TEST ************
+normal 0. 120392 s
+sse128 0. 133654 s
+sse256 0. 120796 s
+********* SUB  TEST ************
+normal 0. 120199 s
+sse128 0. 128295 s
+sse256 0. 124076 s
+********* DIV  TEST ************
+normal 0. 128529 s
+sse128 0. 132324 s
+sse256 0. 136171 s
+!!!Hello World OF Advanced Vector Extensions for : d sizeof  : 8
+Effective Threads Runtime
+********* SQRT TEST ************
+normal_sqrt	 2.09169 s
+sse128_sqrt	 0.60109 s
+sse256_sqrt	 0.56982 s
+********* ADD  TEST ************
+normal_add	 0.27129 s
+sse128_add	 0.26449 s
+sse256_add	 0.26158 s
+********* SUB  TEST ************
+normal_sub	 0.26944 s
+sse128_sub	 0.26666 s
+sse256_sub	 0.29683 s
+********* DIV  TEST ************
+normal_div	 0.26417 s
+sse128_div	 0.34963 s
+sse256_div	 0.26218 s
+
+Effective clock Runtime
+
+Memory 0.     24 s
+Init   2.2699656 s
+********* SQRT TEST ************
+normal 2.2136843 s
+sse128 0. 602878 s
+sse256 0. 570262 s
+********* ADD  TEST ************
+normal 0. 271612 s
+sse128 0. 264537 s
+sse256 0. 261813 s
+********* SUB  TEST ************
+normal 0. 269849 s
+sse128 0. 266866 s
+sse256 0. 297276 s
+********* DIV  TEST ************
+normal 0. 264256 s
+sse128 0. 350930 s
+sse256 0. 262520 s
 
 ```
+
+
 
 ## Modern compiler are integrating vectorization in standard optimization configuration
 
-using g++ and setting -tree-vectorize only
 
-
-
-```
-# with maximum optimization
-!!!Hello World OF Advanced Vector Extensions : Double Precision!!!
-Effective Threads Runtime
- 1.41134	seconds normal
- 0.63199	seconds sse128
- 0.59083	seconds sse256
-Effective clock Runtime
-Normal 1.1416178 s
-SSE128 0.634894 s
-SSE256 0.592923 s
-
-```
-
-interesting to notice that we are a bit lost here because the optimazer has even optimized sse128!
-
-```
-# with maximum optimization and -ttree-vectorize
-!!!Hello World OF Advanced Vector Extensions : Double Precision!!!
-Effective Threads Runtime
- 1.32498	seconds normal
- 0.47684	seconds sse128
- 0.47063	seconds sse256
-Effective clock Runtime
-Normal 1.1325111 s
-SSE128 0.476849 s
-SSE256 0.470666 s
-
-```
+interesting to notice that we are a bit lost here because the optimizer has even optimized sse128! 
--- AVX Test/src/AVX Test Single Precision.cpp
+++ AVX Test/src/AVX Test Single Precision.cpp
@@ -1,9 +1,13 @@
 //============================================================================
-// Name        : AVX.cpp
+// Name        : AVX Test.cpp
 // Author      : rumi
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Hello World in C++, Ansi-style, I always like testing technology with Hello worl appraoch, it becomes rapidely more than just hellow world
+// anyway this has allowed me pushing my C++ rusty knowledge in action, I know  I could have avoided playing with template but  I love it.
+// furthermore the Intel routine for vectorization are not necessarily package the way I like.
+// there is one optimization in the code I may do later is to merge the 128 and 256 routines in the same template :-)
+//
 //============================================================================
 
 #include <iostream>
@@ -15,11 +19,11 @@
 using namespace std::chrono;
 using namespace std;
 
-#define _mm_Test(NAME,INDEX,FUNCTION)\
+#define _mm_Test(NAME,INDEX,FUNCTION,CAST)\
 	auto NAME ## INDEX ## _ ## FUNCTION ## _start = chrono::high_resolution_clock::now();\
 	clock_t c_ ## NAME ## INDEX ## _ ## FUNCTION ## _start = clock();\
 	{\
-		NAME ## INDEX ## _ ## FUNCTION (a, N);\
+		NAME ## INDEX ## _ ## FUNCTION ## CAST(a, N);\
 	}\
 	auto NAME ## INDEX ## _ ## FUNCTION ## _end = chrono::high_resolution_clock::now();\
 	clock_t c_ ## NAME ## INDEX ## _ ## FUNCTION ## _end = clock();
@@ -34,98 +38,201 @@ printf(# NAME # INDEX " %lld.%7lld s\n",\
 printf(# NAME # INDEX "_" # FUNCTION "\t%8.5f s\n",\
 				(double) (c_ ## NAME ## INDEX ## _ ## FUNCTION ## _end - c_ ## NAME ## INDEX ## _ ## FUNCTION ## _start) / CLOCKS_PER_SEC);
 
-
-#define __init(a,N)\
+#define __init(a,N,VALUE)\
 for (int i = 0; i < N; ++i)\
-		a[i] = 3141592.65358;
+		a[i] = VALUE+ i / 10000;
+
+template<typename T, typename U> void mm_store(T t, U u);
+
+template<typename U>
+void mm_store(float* f, U u) {
+	_mm_store_ps(f, u);
+}
+
+template<typename U>
+void mm_store(double* d, U u) {
+	_mm_store_pd(d, u);
+}
+
+template<typename T, typename U> void mm256_store(T t, U u);
+
+template<typename U>
+void mm256_store(float* f, U u) {
+	_mm256_store_ps(f, u);
+}
+
+template<typename U>
+void mm256_store(double* d, U u) {
+	_mm256_store_pd(d, u);
+}
 
 // compile making sure you have support for AVX2 : compiler option -mavx2
 
-void normal_sqrt(float* a, int N) {
-/*#pragma nounroll
-#pragma clang loop interleave(disable)
-#pragma clang loop vectorize(disable)*/
+template<typename T>
+void normal_sqrt(T* a, int N) {
 	for (int i = 0; i < N; ++i)
 		a[i] = sqrt(a[i]);
 }
 
-
-void normal_add(float* a, int N) {
-#pragma nounroll
-#pragma clang loop interleave(disable)
-#pragma clang loop vectorize(disable)
-	for (int i = 0; i < N/2; ++i)
-		a[i] = a[i] + a[N/2+i];
+template<typename T>
+void normal_add(T* a, int N) {
+	for (int i = 0; i < N / 2; ++i)
+		a[i] = a[i] + a[N / 2 + i];
 }
 
-void sse128_sqrt(float* a, int N) {
+template<typename T>
+void normal_sub(T* a, int N) {
+	for (int i = 0; i < N / 2; ++i)
+		a[i] = a[i] - a[N / 2 + i];
+}
+
+template<typename T>
+void normal_div(T* a, int N) {
+	for (int i = 0; i < N / 2; ++i)
+		a[i] = a[i] / a[N / 2 + i];
+}
+
+template<typename T>
+void sse128_sqrt(T* a, int N) {
 	// We assume N % 4 == 0.
-	int nb_iters = N / 4;
+	int nb_iters = N * sizeof(T) / 16;
 	__m128* ptr = (__m128*)a;
 
-	for (int i = 0; i < nb_iters; ++i, ++ptr, a += 4)
-		_mm_store_ps(a, _mm_sqrt_ps(*ptr));
+	for (int i = 0; i < nb_iters; ++i, ++ptr, a += 16 / sizeof(T))
+		mm_store(a,
+				std::is_same<float, T>::value ?
+						_mm_sqrt_ps(*ptr) : _mm_sqrt_pd(*ptr));
 }
-
-void sse128_add(float* a, int N) {
+template<typename T>
+void sse128_add(T* a, int N) {
 	// We assume N % 4 == 0.
-	int nb_iters = N / 4 / 2;
+	int nb_iters = N * sizeof(T) / 16 / 2;
 	__m128* ptr = (__m128*)a;
 	__m128* ptr2 = (__m128*) &(a[N/2]);
 
-	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 8)
-		_mm_store_ps(a, _mm_add_ps(*ptr, *ptr2));
+	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 16 / sizeof(T))
+		mm_store(a,
+				std::is_same<float, T>::value ?
+						_mm_add_ps(*ptr, *ptr2) : _mm_add_pd(*ptr, *ptr2));
 }
 
-
-void sse256_sqrt(float* a, int N) {
+template<typename T>
+void sse128_sub(T* a, int N) {
 	// We assume N % 4 == 0.
-	int nb_iters = N / 8;
+	int nb_iters = N * sizeof(T) / 16 / 2;
+	__m128* ptr = (__m128*)a;
+	__m128* ptr2 = (__m128*) &(a[N/2]);
+
+	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 16 / sizeof(T))
+		mm_store(a,
+				std::is_same<float, T>::value ?
+						_mm_sub_ps(*ptr, *ptr2) : _mm_sub_pd(*ptr, *ptr2));
+}
+
+template<typename T>
+void sse128_div(T* a, int N) {
+	// We assume N % 4 == 0.
+	int nb_iters = N * sizeof(T) / 16 / 2;
+	__m128* ptr = (__m128*)a;
+	__m128* ptr2 = (__m128*) &(a[N/2]);
+
+	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 16 / sizeof(T))
+		mm_store(a,
+				std::is_same<float, T>::value ?
+						_mm_div_ps(*ptr, *ptr2) : _mm_div_pd(*ptr, *ptr2));
+}
+template<typename T>
+void sse256_sqrt(T* a, int N) {
+	// We assume N % 4 == 0.
+	int nb_iters = N * sizeof(T) / 32;
 	__m256* ptr = (__m256*)a;
 
-	for (int i = 0; i < nb_iters; ++i, ++ptr, a += 8)
-		_mm256_store_ps(a, _mm256_sqrt_ps(*ptr));
+	for (int i = 0; i < nb_iters; ++i, ++ptr, a += 32 / sizeof(T))
+		mm256_store(a,
+				std::is_same<float, T>::value ?
+						_mm256_sqrt_ps(*ptr) : _mm256_sqrt_pd(*ptr));
 }
 
-void sse256_add(float* a, int N) {
+template<typename T>
+void sse256_add(T* a, int N) {
 	// We assume N % 4 == 0.
-	int nb_iters = N / 8 / 2;
+	int nb_iters = N * sizeof(T) / 32 / 2;
 	__m256* ptr = (__m256*)a;
 	__m256* ptr2 = (__m256*) &(a[N/2]);
 
-	for (int i = 0; i < nb_iters; ++i, ++ptr,++ptr2, a += 8)
-		_mm256_store_ps(a, _mm256_add_ps(*ptr, *ptr2));
+	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 32 / sizeof(T))
+		mm256_store(a,
+				std::is_same<float, T>::value ?
+						_mm256_add_ps(*ptr, *ptr2) :
+						_mm256_add_pd(*ptr, *ptr2));
 }
-int main(int argc, char** argv) {
-	if (argc != 2)
-		return 1;
-	int N = atoi(argv[1]);
-	cout
-			<< "!!!Hello World OF Advanced Vector Extensions : Single Precision Test!!!"
-			<< endl;
-	float* a;
 
+template<typename T>
+void sse256_sub(T* a, int N) {
+	// We assume N % 4 == 0.
+	int nb_iters = N * sizeof(T) / 32 / 2;
+	__m256* ptr = (__m256*)a;
+	__m256* ptr2 = (__m256*) &(a[N/2]);
+
+	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 32 / sizeof(T))
+		mm256_store(a,
+				std::is_same<float, T>::value ?
+						_mm256_sub_ps(*ptr, *ptr2) :
+						_mm256_sub_pd(*ptr, *ptr2));
+}
+
+template<typename T>
+void sse256_div(T* a, int N) {
+	// We assume N % 4 == 0.
+	int nb_iters = N * sizeof(T) / 32 / 2;
+	__m256* ptr = (__m256*)a;
+	__m256* ptr2 = (__m256*) &(a[N/2]);
+
+	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 32 / sizeof(T))
+		mm256_store(a,
+				std::is_same<float, T>::value ?
+						_mm256_div_ps(*ptr, *ptr2) :
+						_mm256_div_pd(*ptr, *ptr2));
+}
+
+template<typename T>
+void GlobalTest(int N) {
+	T* a;
+	cout << "!!!Hello World OF Advanced Vector Extensions for : "
+			<< typeid(T).name() << " sizeof  : " << sizeof(T) << endl;
 	auto alloc_start = chrono::high_resolution_clock::now();
 
-	posix_memalign((void**) &a, 64, N * sizeof(float));
+	posix_memalign((void**) &a, 64, N * sizeof(T));
 	auto alloc_end = chrono::high_resolution_clock::now();
 
-
 	auto init_start = chrono::high_resolution_clock::now();
-	__init(a,N)
+	__init(a, N, 3141592.65358)
 	auto init_end = chrono::high_resolution_clock::now();
+	_mm_Test(normal,, sqrt,)
+	__init(a, N, 3141592.65358)
+	_mm_Test(sse, 128, sqrt,)
+	__init(a, N, 3141592.65358)
+	_mm_Test(sse, 256, sqrt,)
+	__init(a, N, 3141592.65358)
+	_mm_Test(normal,, add,);
+	__init(a, N, 3141592.65358)
+	_mm_Test(sse, 128, add,);
+	__init(a, N, 3141592.65358)
+	_mm_Test(sse, 256, add,);
+	__init(a, N, 3141592.65358)
+	_mm_Test(normal,, sub,);
+	__init(a, N, 3141592.65358)
+	_mm_Test(sse, 128, sub,);
+	__init(a, N, 3141592.65358)
+	_mm_Test(sse, 256, sub,);
 
-	_mm_Test(normal,, sqrt)
-	__init(a,N)
-	_mm_Test(sse, 128, sqrt)
-	__init(a,N)
-	_mm_Test(sse, 256, sqrt)
-	__init(a,N)
-	_mm_Test(normal, , add);
-	__init(a,N)
-	_mm_Test(sse, 128, add);
-	__init(a,N)
-	_mm_Test(sse, 256, add);
+	__init(a, N, 3141592.65358)
+	_mm_Test(normal,, div,);
+	__init(a, N, 3141592.65358)
+
+	_mm_Test(sse, 128, div,);
+	__init(a, N, 3141592.65358)
+	_mm_Test(sse, 256, div,);
 
 	printf("Effective Threads Runtime\n");
 	printf("********* SQRT TEST ************\n");
@@ -136,6 +243,16 @@ int main(int argc, char** argv) {
 	_mm_print_Test_runtime(normal,, add)
 	_mm_print_Test_runtime(sse, 128, add)
 	_mm_print_Test_runtime(sse, 256, add)
+
+	printf("********* SUB  TEST ************\n");
+	_mm_print_Test_runtime(normal,, sub)
+	_mm_print_Test_runtime(sse, 128, sub)
+	_mm_print_Test_runtime(sse, 256, sub)
+
+	printf("********* DIV  TEST ************\n");
+	_mm_print_Test_runtime(normal,, div)
+	_mm_print_Test_runtime(sse, 128, div)
+	_mm_print_Test_runtime(sse, 256, div)
 
 	printf("\nEffective clock Runtime\n\n");
 
@@ -153,6 +270,25 @@ int main(int argc, char** argv) {
 	_mm_print_clock_time(normal,, add)
 	_mm_print_clock_time(sse, 128, add)
 	_mm_print_clock_time(sse, 256, add)
+
+	printf("********* SUB  TEST ************\n");
+	_mm_print_clock_time(normal,, sub)
+	_mm_print_clock_time(sse, 128, sub)
+	_mm_print_clock_time(sse, 256, sub)
+
+	printf("********* DIV  TEST ************\n");
+	_mm_print_clock_time(normal,, div)
+	_mm_print_clock_time(sse, 128, div)
+	_mm_print_clock_time(sse, 256, div)
+
+}
+int main(int argc, char** argv) {
+	if (argc != 2)
+		return 1;
+	int N = atoi(argv[1]);
+
+	GlobalTest<float>(N);
+	GlobalTest<double>(N);
 
 }
 
