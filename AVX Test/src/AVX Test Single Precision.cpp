258a10069986a7a3eeafd0f9c8724b5861117d74
rumi <rumi@RUMI-P310>
2017-06-12 22:41:11 -0400

    upd

--- AVX Test/src/AVX Test Single Precision.cpp
+++ AVX Test/src/AVX Test Single Precision.cpp
@@ -15,6 +15,7 @@
 #include <immintrin.h> // for _m256
 #include <math.h>
 #include <chrono>
+#include <typeinfo>
 
 using namespace std::chrono;
 using namespace std;
@@ -51,7 +52,7 @@ void mm_store(float* f, U u) {
 
 template<typename U>
 void mm_store(double* d, U u) {
-	_mm_store_pd(d, u);
+	_mm_store_pd(d, ( __m128d )u);
 }
 
 template<typename T, typename U> void mm256_store(T t, U u);
@@ -63,7 +64,7 @@ void mm256_store(float* f, U u) {
 
 template<typename U>
 void mm256_store(double* d, U u) {
-	_mm256_store_pd(d, u);
+	_mm256_store_pd(d,( __m256d ) u);
 }
 
 // compile making sure you have support for AVX2 : compiler option -mavx2
@@ -100,8 +101,12 @@ void sse128_sqrt(T* a, int N) {
 
 	for (int i = 0; i < nb_iters; ++i, ++ptr, a += 16 / sizeof(T))
 		mm_store(a,
-				std::is_same<float, T>::value ?
-						_mm_sqrt_ps(*ptr) : _mm_sqrt_pd(*ptr));
+				#if float == T
+				 _mm_sqrt_ps((__m128 )*ptr)
+#else
+	 _mm_sqrt_pd((__m128d )*ptr)
+#endif
+		);
 }
 template<typename T>
 void sse128_add(T* a, int N) {
@@ -112,8 +117,12 @@ void sse128_add(T* a, int N) {
 
 	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 16 / sizeof(T))
 		mm_store(a,
-				std::is_same<float, T>::value ?
-						_mm_add_ps(*ptr, *ptr2) : _mm_add_pd(*ptr, *ptr2));
+#if float == T
+						_mm_add_ps((__m128 )*ptr, (__m128 )*ptr2)
+#else
+						_mm_add_pd((__m128d )*ptr, (__m128d )*ptr2)
+#endif
+						);
 }
 
 template<typename T>
@@ -125,8 +134,12 @@ void sse128_sub(T* a, int N) {
 
 	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 16 / sizeof(T))
 		mm_store(a,
-				std::is_same<float, T>::value ?
-						_mm_sub_ps(*ptr, *ptr2) : _mm_sub_pd(*ptr, *ptr2));
+#if float == T
+				_mm_sub_ps(*ptr, *ptr2)
+#else
+				_mm_sub_pd((__m128d )*ptr, (__m128d )*ptr2)
+#endif
+				);
 }
 
 template<typename T>
@@ -138,8 +151,12 @@ void sse128_div(T* a, int N) {
 
 	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 16 / sizeof(T))
 		mm_store(a,
-				std::is_same<float, T>::value ?
-						_mm_div_ps(*ptr, *ptr2) : _mm_div_pd(*ptr, *ptr2));
+#if float == T
+				_mm_div_ps(*ptr, *ptr2)
+#else
+				_mm_div_pd((__m128d )*ptr, (__m128d )*ptr2)
+#endif
+				);
 }
 template<typename T>
 void sse256_sqrt(T* a, int N) {
@@ -149,8 +166,12 @@ void sse256_sqrt(T* a, int N) {
 
 	for (int i = 0; i < nb_iters; ++i, ++ptr, a += 32 / sizeof(T))
 		mm256_store(a,
-				std::is_same<float, T>::value ?
-						_mm256_sqrt_ps(*ptr) : _mm256_sqrt_pd(*ptr));
+#if float == T
+				_mm256_sqrt_ps((__m256 )*ptr)
+#else
+				_mm256_sqrt_pd((__m256d )*ptr
+#endif
+						);
 }
 
 template<typename T>
@@ -162,9 +183,11 @@ void sse256_add(T* a, int N) {
 
 	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 32 / sizeof(T))
 		mm256_store(a,
-				std::is_same<float, T>::value ?
-						_mm256_add_ps(*ptr, *ptr2) :
-						_mm256_add_pd(*ptr, *ptr2));
+#if float == T
+				_mm256_add_ps((__m256 )*ptr, (__m256 )*ptr2)
+#else
+#endif
+				);
 }
 
 template<typename T>
@@ -176,9 +199,12 @@ void sse256_sub(T* a, int N) {
 
 	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 32 / sizeof(T))
 		mm256_store(a,
-				std::is_same<float, T>::value ?
-						_mm256_sub_ps(*ptr, *ptr2) :
-						_mm256_sub_pd(*ptr, *ptr2));
+#if float == T
+				_mm256_sub_ps(*ptr, *ptr2)
+#else
+						_mm256_sub_pd((__m256d )*ptr, (__m256d )*ptr2)
+#endif
+						);
 }
 
 template<typename T>
@@ -190,9 +216,12 @@ void sse256_div(T* a, int N) {
 
 	for (int i = 0; i < nb_iters; ++i, ++ptr, ++ptr2, a += 32 / sizeof(T))
 		mm256_store(a,
-				std::is_same<float, T>::value ?
-						_mm256_div_ps(*ptr, *ptr2) :
-						_mm256_div_pd(*ptr, *ptr2));
+#if float == T
+				_mm256_div_ps(*ptr, *ptr2)
+#else
+						_mm256_div_pd((__m256d )*ptr, (__m256d )*ptr2)
+#endif
+						);
 }
 
 template<typename T>
