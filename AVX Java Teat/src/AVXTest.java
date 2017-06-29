f52599aa3cfe541b7f6d87f17acbc0a83c345922
bigleux <bigleuxenchef@gmail.com>
2017-06-29 16:21:55 -0400

    upd

--- AVX Java Teat/src/AVXTest.java
+++ AVX Java Teat/src/AVXTest.java
@@ -1,157 +1,188 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 
+// JM options
+// -ea
+//-XX:+UseSuperWord
+// -XX:UseAVX=1
+// -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -XX:+LogCompilation
+// -XX:+PrintGC
+// -Xprof
 
-
+//-XX:UseSSE=3
+//-XX:+UseSuperWord
 
 public class AVXTest {
 	static int N = 51200000;
-    @Rule public TestName name = new TestName();
+	static long TimeDuration;
+	static int i=0;
 	
 	@Rule
+	public TestName name = new TestName();
+
+	@Rule
 	public RunInThreadRule runInThread = new RunInThreadRule();
-	
-	
-	
-	@BeforeClass public static void initialize() {
+
+	@BeforeClass
+	public static void initialize() {
 		System.out.printf("\n\n!!!Hello World OF Advanced Vector Extensions for : iteration : %d \n", N);
 
-	    }
-		
+	}
+	
+	@After
+	public void aftermethod()
+	{
+		System.out.printf("\n%s => ", name.getMethodName());
+		System.out.printf("Java/Inline %s      Results Iteration # %d duration %d millis result\n",name.getMethodName().matches(".*_d")? "Double":"Float", i,
+				TimeDuration);
+
+	}
+	
+
 	@Test
-	@RunInThread
+	//@RunInThread
 	public void test_normal_add() {
-		int i;
 		float[] a = new float[N];
-		
-		System.out.printf("\n%s => ",name.getMethodName());
 
-			for (i = 0; i < N; ++i)
-				{a[i] = (float) 3141592.653588 + i;
-				}
-
-			long timeStart = System.currentTimeMillis();
-
-			SimpleThreads.normal_add(a, N);
-
-			long TimeDuration = System.currentTimeMillis() - timeStart;
-
-			System.out.printf("Java/Inline Float      Results Iteration # %d duration %d millis resultd\n",i,
-					TimeDuration);
-			
-		
-			
+		for (i = 0; i < N; ++i) {
+			a[i] = (float) 3141592.653588 + i;
 		}
+
+		long timeStart = System.currentTimeMillis();
+
+		SimpleThreads.normal_add(a, N);
+
+		TimeDuration = System.currentTimeMillis() - timeStart;
+
+	}
+
 	@Test
-	@RunInThread
+	//@RunInThread
 	public void test_normal_sub() {
-		int i;
 		float[] a = new float[N];
 
-		System.out.printf("\n%s => ",name.getMethodName());
-
-
-			for (i = 0; i < N; ++i)
-				{a[i] = (float) 3141592.653588 + i;
-				}
-
-			long timeStart = System.currentTimeMillis();
-
-			SimpleThreads.normal_sub(a, N);
-
-			long TimeDuration = System.currentTimeMillis() - timeStart;
-
-			System.out.printf("Java/Inline Float      Results Iteration # %d duration %d millis resultd\n", i,
-					TimeDuration);
-			
-		
-			
+		for (i = 0; i < N; ++i) {
+			a[i] = (float) 3141592.653588 + i;
 		}
-	
-	
+
+		long timeStart = System.currentTimeMillis();
+
+		SimpleThreads.normal_sub(a, N);
+
+		TimeDuration = System.currentTimeMillis() - timeStart;
+
+	}
+
 	@Test
-	@RunInThread
+	//@RunInThread
 	public void test_normal_add_d() {
-		int i;
 		double[] a = new double[N];
 
-		System.out.printf("\n%s => ",name.getMethodName());
-
-
-
-			for (i = 0; i < N; ++i)
-				{a[i] = (double) 3141592.653588 + i;
-				}
-
-			long timeStart = System.currentTimeMillis();
-
-			SimpleThreads.normal_add_d(a, N);
-
-			long TimeDuration = System.currentTimeMillis() - timeStart;
-
-			System.out.printf("Java/Inline Double      Results Iteration # %d duration %d millis resultd\n", i,
-					TimeDuration);
-			
-		
-			
+		for (i = 0; i < N; ++i) {
+			a[i] = (double) 3141592.653588 + i;
 		}
+
+		long timeStart = System.currentTimeMillis();
+
+		SimpleThreads.normal_add_d(a, N);
+
+		TimeDuration = System.currentTimeMillis() - timeStart;
+
 	
+
+	}
+
 	@Test
-	@RunInThread
+	//@RunInThread
 	public void test_normal_sub_d() {
-		int i;
 		double[] a = new double[N];
 
-		System.out.printf("\n%s => ",name.getMethodName());
-
-
-			for (i = 0; i < N; ++i)
-				{a[i] = (double) 3141592.653588 + i;
-				}
-
-			long timeStart = System.currentTimeMillis();
-
-			SimpleThreads.normal_sub_d(a, N);
-
-			long TimeDuration = System.currentTimeMillis() - timeStart;
-
-			System.out.printf("Java/Inline Double      Results Iteration # %d duration %d millis resultd\n", i,
-					TimeDuration);
-			
-		
-			
+		for (i = 0; i < N; ++i) {
+			a[i] = (double) 3141592.653588 + i;
 		}
-	
-	
+
+		long timeStart = System.currentTimeMillis();
+
+		SimpleThreads.normal_sub_d(a, N);
+
+		TimeDuration = System.currentTimeMillis() - timeStart;
+
+	}
+
 	@Test
 	@RunInThread
 	public void test_normal_subT() {
-		int i;
 		Number[] b = new Number[N];
 
-		System.out.printf("\n%s => ",name.getMethodName());
-
-
-			for (i = 0; i < N; ++i)
-				{b[i] = (double) 3141592.653588 + i;
-				}
-
-			long timeStart = System.currentTimeMillis();
-
-			SimpleThreads.normal_subT(b, N);
-
-			long TimeDuration = System.currentTimeMillis() - timeStart;
-
-			System.out.printf("Java/Inline Double/Template       Results Iteration # %d duration %d millis resultd\n", i,
-					TimeDuration);
-			
-		
-			
+		for (i = 0; i < N; ++i) {
+			b[i] = (float) (3141592.653588 + i);
 		}
-		
-	}
 
+		long timeStart = System.currentTimeMillis();
+
+		SimpleThreads.normal_subT(b, N);
+
+		TimeDuration = System.currentTimeMillis() - timeStart;
+
+
+	}
+	
+	@Test //(timeout=1000)
+	@RunInThread
+	public void test_normal_subT_d() {
+		Number[] b = new Number[N];
+
+		for (i = 0; i < N; ++i) {
+			b[i] = (double) (3141592.653588 + i);
+		}
+
+		long timeStart = System.currentTimeMillis();
+
+		SimpleThreads.normal_subT(b, N);
+
+		TimeDuration = System.currentTimeMillis() - timeStart;
+
+
+	}
+	
+	
+	@Test
+	@RunInThread
+	public void test_normal_addT() {
+		Number[] b = new Number[N];
+
+		for (i = 0; i < N; ++i) {
+			b[i] = (float) (3141592.653588 + i);
+		}
+
+		long timeStart = System.currentTimeMillis();
+
+		SimpleThreads.normal_addT(b, N);
+
+		TimeDuration = System.currentTimeMillis() - timeStart;
+
+
+	}
+	@Test
+	@RunInThread
+	public void test_normal_addT_d() {
+		Number[] b = new Number[N];
+
+		for (i = 0; i < N; ++i) {
+			b[i] = (double) (3141592.653588 + i);
+		}
+
+		long timeStart = System.currentTimeMillis();
+
+		SimpleThreads.normal_addT(b, N);
+
+		TimeDuration = System.currentTimeMillis() - timeStart;
+
+
+	}
+}
--- AVX Java Teat/src/SimpleThreads.java
+++ AVX Java Teat/src/SimpleThreads.java
@@ -8,42 +8,43 @@ public class SimpleThreads {
 		for (int i = 0; i < N / 2; ++i)
 			a[i] = a[i] + a[N / 2 + i];
 	}
-	
+
 	static void normal_add_d(double a[], int N) {
 		for (int i = 0; i < N / 2; ++i)
 			a[i] = a[i] + a[N / 2 + i];
 	}
-	
-	
+
 	static void normal_sub(float a[], int N) {
 		for (int i = 0; i < N / 2; ++i)
 			a[i] = a[i] - a[N / 2 + i];
 	}
-	
+
 	static void normal_sub_d(double a[], int N) {
 		for (int i = 0; i < N / 2; ++i)
 			a[i] = a[i] - a[N / 2 + i];
 	}
-	
-	
-	static <T extends Number>void normal_subT(T a[], int N) {
+
+	static <T extends Number> void normal_subT(T a[], int N) {
 		for (int i = 0; i < N / 2; ++i)
-			if (a[0] instanceof Float)
-				  {
-						 a[i] = (T) (Number) (a[i].floatValue() - a[N / 2 + i].floatValue());
-				  }
-			else
-				if (a[0] instanceof Double)
-				{
-					a[i] = (T) (Number)(a[i].doubleValue() - a[N / 2 + i].doubleValue());
-				}
+			if (a[0] instanceof Float) {
+				// System.out.printf("Float\n");
+				a[i] = (T) (Number) (a[i].floatValue() - a[N / 2 + i].floatValue());
+			} else if (a[0] instanceof Double) {
+				// System.out.printf("Double\n");
+				a[i] = (T) (Number) (a[i].doubleValue() - a[N / 2 + i].doubleValue());
+			}
 
 	}
 
-	
-	
-	
-	
+	static <T extends Number> void normal_addT(T a[], int N) {
+		if (a[0] instanceof Float)
+			for (int i = 0; i < N / 2; ++i)
+				a[i] = (T) (Number) (a[i].floatValue() + a[N / 2 + i].floatValue());
+		else if (a[0] instanceof Double)
+			for (int i = 0; i < N / 2; ++i)
+				a[i] = (T) (Number) (a[i].doubleValue() + a[N / 2 + i].doubleValue());
+
+	}
 
 	private static class TestLoop implements Runnable {
 		public void run() {
@@ -55,10 +56,10 @@ public class SimpleThreads {
 			System.out.printf("\n\n!!!Hello World OF Advanced Vector Extensions for : iteration : %d \n", N);
 
 			for (int j = 1; j < 100; j++) {
-				for (i = 0; i < N; ++i)
-					{a[i] = (float) 3141592.653588 + i;
-					b[i] = (double)3141592.653588 + i;
-					}
+				for (i = 0; i < N; ++i) {
+					a[i] = (float) 3141592.653588 + i;
+					b[i] = (double) 3141592.653588 + i;
+				}
 
 				long timeStart = System.currentTimeMillis();
 
@@ -68,30 +69,27 @@ public class SimpleThreads {
 
 				System.out.printf("\nJava/Inline Float      Results Iteration # %d duration %d millis resultd\n", i,
 						TimeDuration);
-				
-				 timeStart = System.currentTimeMillis();
+
+				timeStart = System.currentTimeMillis();
 
 				normal_subT(b, N);
 
 				TimeDuration = System.currentTimeMillis() - timeStart;
 
-				System.out.printf("\nJava/Inline Double/Template       Results Iteration # %d duration %d millis resultd\n", i,
+				System.out.printf(
+						"\nJava/Inline Double/Template       Results Iteration # %d duration %d millis resultd\n", i,
 						TimeDuration);
-				
-				
-				
+
 			}
 		}
 
 	}
 
-	
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Thread t = new Thread(new TestLoop());
-	
+
 		t.start();
 	}
 }
